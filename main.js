7104592dfd3bf076c5838fc0c1c2780ffba03dd6
noeliaromga@gmail.com <noeliaromga@gmail.com>
2022-03-22 11:10:27 +0100

    excercises conditionals

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 "use strict"; 
 console.log ('Hola');
 const formElement = document.querySelector('.new-form');
-formElement.classList.remove('collapsed');
+ 
+ if (formElement.classList.contains('collapsed')) {
+  formElement.classList.remove('collapsed');
+} else {
+  formElement.classList.add('collapsed');
+}
 
 const listElement= document.querySelector(".list");
-
 
 const kittenImage1 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
 const kittenName1 = "Anastacio";
@@ -17,7 +21,7 @@ const kittenImage2 = "https://images.emedicinehealth.com/images/article/main_ima
 const kittenName2 = "Fiona";
 const nameUpperCase2 = kittenName2.toUpperCase();
 const kittenDesc2 =
-  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+  "Ruiseño, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRace2 = "British Shorthair";
 
 const kittenImage3 = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
@@ -27,11 +31,23 @@ const kittenDesc3 =
   "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRace3 = "British Shorthair";
 
-const content1 = `<li class="card"><article><img class="card_img" src="${kittenImage1}" alt="gatito"/><h3 class="card_title">${nameUpperCase1}</h3><h4 class="card_race">${kittenRace1}</h4><p class="card_description">${kittenDesc1} </p></article></li>`;
+const input_search_desc = document.querySelector('.js_in_search_desc');
+const descrSearchText = input_search_desc.value;
 
+if( kittenDesc1.includes(descrSearchText)  ) {
+  const content1 = `<li class="card"><article><img class="card_img" src="${kittenImage1}" alt="gatito"/><h3 class="card_title">${nameUpperCase1}</h3><h4 class="card_race">${kittenRace1}</h4><p class="card_description">${kittenDesc1} </p></article></li>`;
+  listElement.innerHTML = content1;
+}
+
+
+if( kittenDesc2.includes(descrSearchText)  ) {
 const content2 = `<li class="card"><article><img class="card_img" src="${kittenImage2}" alt="gatito"/><h3 class="card_title">${nameUpperCase2}</h3><h4 class="card_race">${kittenRace2}</h4><p class="card_description">${kittenDesc2} </p> </article></li>`;
+listElement.innerHTML = listElement.innerHTML + content2;
+}
 
+
+if( kittenDesc3.includes(descrSearchText)  ) {
 const content3 = `<li class="card"><article><img class="card_img" src="${kittenImage3}" alt="gatito"/><h3 class="card_title">${nameUpperCase3}</h3><h4 class="card_race">${kittenRace3}</h4><p class="card_description">${kittenDesc3} </p></article></li>`;
-
-listElement.innerHTML= `${content1} ${content2} ${content3}`;
+listElement.innerHTML = listElement.innerHTML + content3;
+}
 
