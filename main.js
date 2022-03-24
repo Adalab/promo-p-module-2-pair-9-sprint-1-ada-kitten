2f778642b0abae9f3795f0268dc43c15c3d06e2b
“CarlotaBlanco” <lotsilots@gmail.com>
2022-03-24 11:55:12 +0100

    AdaKitten functions

--- main.js
+++ main.js
@@ -1,19 +1,62 @@
 "use strict"; 
-console.log ('Hola');
 const formElement = document.querySelector('.new-form');
  
- /* if (formElement.classList.contains('collapsed')) {
+ /*if (formElement.classList.contains('collapsed')) {
   formElement.classList.remove('collapsed');
 } else {
   formElement.classList.add('collapsed');
 } */
 
 
-const item = document.querySelector('.item');
-item.addEventListener("click", (event) =>{
+const item = document.querySelector('.js-item');
+
+function handleClickNewCatForm(event) {
   event.preventDefault();
   formElement.classList.toggle('collapsed');
-}); //toggle cuando la das la pone o la quita (como add / remove) 
+}
+item.addEventListener('click', handleClickNewCatForm);
+
+
+
+
+const kittenImage1 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
+const kittenName1 = "Anastacio";
+const nameUpperCase1 = kittenName1.toUpperCase();
+const kittenDesc1 =
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenRace1 = "British Shorthair";
+const listElement= document.querySelector(".list");
+const kittenImage2 = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" ;
+const kittenName2 = "Fiona";
+const nameUpperCase2 = kittenName2.toUpperCase();
+const kittenDesc2 =
+  "Ruiseño, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenRace2 = "British Shorthair";
+
+const kittenImage3 = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
+const kittenName3 = "Cielo";
+const nameUpperCase3 = kittenName3.toUpperCase();
+const kittenDesc3 =
+  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
+const kittenRace3 = "British Shorthair";
+
+function renderKitten(url, desc, name, race) {
+  console.log('michi');
+  
+  return `<li class="card"><article><img class="card_img" src="${url}" alt="gatito"/><h3 class="card_title">${name}</h3><h4 class="card_race">${race}</h4><p class="card_description">${desc} </p></article></li>`;
+}
+
+
+listElement.innerHTML += renderKitten(kittenImage1, kittenDesc1, kittenName1, kittenRace1);
+listElement.innerHTML += renderKitten(kittenImage2, kittenDesc2, kittenName2, kittenRace2);
+listElement.innerHTML += renderKitten(kittenImage3, kittenDesc3, kittenName3, kittenRace3);
+
+
+
+/*item.addEventListener("click", (event) =>{
+  event.preventDefault();
+  formElement.classList.toggle('collapsed');
+}); */ //toggle cuando la das la pone o la quita (como add / remove) 
 
 const inputDesc = document.querySelector(".js-input-desc");
 const inputPhoto = document.querySelector(".js-input-photo");
@@ -55,28 +98,9 @@ search.addEventListener ("click", (event) =>{
   });
 
 
-const listElement= document.querySelector(".list");
+/*const listElement= document.querySelector(".list");*/
 
-const kittenImage1 = "https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg";
-const kittenName1 = "Anastacio";
-const nameUpperCase1 = kittenName1.toUpperCase();
-const kittenDesc1 =
-  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
-const kittenRace1 = "British Shorthair";
 
-const kittenImage2 = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" ;
-const kittenName2 = "Fiona";
-const nameUpperCase2 = kittenName2.toUpperCase();
-const kittenDesc2 =
-  "Ruiseño, cariñoso, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
-const kittenRace2 = "British Shorthair";
-
-const kittenImage3 = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg";
-const kittenName3 = "Cielo";
-const nameUpperCase3 = kittenName3.toUpperCase();
-const kittenDesc3 =
-  "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
-const kittenRace3 = "British Shorthair";
 
 const input_search_desc = document.querySelector('.js_in_search_desc');
 const descrSearchText = input_search_desc.value;
