d4715bba63883abe01c6ba51268b97d8aafdf9cf
Noelia R <noeliaromga@gmail.com>
2022-03-25 11:23:52 +0100

    excercises objects

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ const nameUpperCase1 = kittenName1.toUpperCase();
 const kittenDesc1 =
   "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRace1 = "British Shorthair";
+
 const listElement= document.querySelector(".list");
 const kittenImage2 = "https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg" ;
 const kittenName2 = "Fiona";
@@ -40,11 +41,11 @@ const kittenDesc3 =
   "Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!";
 const kittenRace3 = "British Shorthair";
 
-function renderKitten(url, desc, name, race) {
+/*function renderKitten(url, desc, name, race) {
   console.log('michi');
   
   return `<li class="card"><article><img class="card_img" src="${url}" alt="gatito"/><h3 class="card_title">${name}</h3><h4 class="card_race">${race}</h4><p class="card_description">${desc} </p></article></li>`;
-}
+}*/
 
 
 listElement.innerHTML += renderKitten(kittenImage1, kittenDesc1, kittenName1, kittenRace1);
@@ -100,25 +101,56 @@ search.addEventListener ("click", (event) =>{
 
 /*const listElement= document.querySelector(".list");*/
 
+function renderKitten(kittenData) {
+  
+  return `<li class="card"><article><img class="card_img" src="${kittenData.image}" alt="gatito"/><h3 class="card_title">${kittenData.name}</h3><h4 class="card_race">${kittenData.race}</h4><p class="card_description">${kittenData.desc} </p></article></li>`;
+}
 
 
 const input_search_desc = document.querySelector('.js_in_search_desc');
 const descrSearchText = input_search_desc.value;
 
+
+
+
+
+//ejemplo de un objeto con la información del primer gatito
+const kittenData_1 = {
+  image: 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg',
+  name: 'Anastacio',
+  desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
+  race: 'British Shorthair',
+};
+
+const kittenData_2 = {
+  image: 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg',
+  name: 'Fiona',
+  desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
+  race: 'British Shorthair',
+};
+
+
+const kittenData_3 = {
+  image: 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg',
+  name: 'Cielo',
+  desc: 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!',
+  race: 'British Shorthair',
+};
+
 if( kittenDesc1.includes(descrSearchText)  ) {
-  const content1 = `<li class="card"><article><img class="card_img" src="${kittenImage1}" alt="gatito"/><h3 class="card_title">${nameUpperCase1}</h3><h4 class="card_race">${kittenRace1}</h4><p class="card_description">${kittenDesc1} </p></article></li>`;
+  const content1 = renderKitten(kittenData_1);
   listElement.innerHTML = content1;
 }
 
 
 if( kittenDesc2.includes(descrSearchText)  ) {
-const content2 = `<li class="card"><article><img class="card_img" src="${kittenImage2}" alt="gatito"/><h3 class="card_title">${nameUpperCase2}</h3><h4 class="card_race">${kittenRace2}</h4><p class="card_description">${kittenDesc2} </p> </article></li>`;
+const content2 = renderKitten(kittenData_2);
 listElement.innerHTML = listElement.innerHTML + content2;
 }
 
 
 if( kittenDesc3.includes(descrSearchText)  ) {
-const content3 = `<li class="card"><article><img class="card_img" src="${kittenImage3}" alt="gatito"/><h3 class="card_title">${nameUpperCase3}</h3><h4 class="card_race">${kittenRace3}</h4><p class="card_description">${kittenDesc3} </p></article></li>`;
+const content3 = renderKitten(kittenData_3);
 listElement.innerHTML = listElement.innerHTML + content3;
 }
 
